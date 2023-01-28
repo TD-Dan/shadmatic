01129920a2086e5150b7ce741a93f74329852771
Daniel Herkert <daniel.herkert@gmail.com>
2023-01-28 20:26:08 +0200

    fixed docstrings to right place

--- shadowui/windowbase.py
+++ shadowui/windowbase.py
@@ -1,14 +1,12 @@
 
 from .section import Section
 
-"""Interface base class for all os level windows."""
 class WindowBase(Section):    
-    """Run the program main loop"""
+    """Interface base class for all os level windows."""
     def run(self):
+        """Run the program main loop"""
         # send load signals from leaf up, children should always be loaded first
         self.emit_signal_recursive_leaf_first("on_load")
-
-    def add_tool(self,page): raise NotImplementedError('add_tool method not implemented by subclass.')
 
     def __init__(self, name: str, **kwargs) -> None:
         super().__init__(name, **kwargs)
