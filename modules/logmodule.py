d09d9f6b348878e87dac169e4e9f4336f9b4b2cc
Daniel Herkert <daniel.herkert@gmail.com>
2023-02-08 22:29:10 +0200

    added write command

--- modules/logmodule.py
+++ modules/logmodule.py
@@ -5,6 +5,8 @@ Provides in-program event viewing
 import state
 from shadowui import Section, Label
 from modules import ModuleBase
+from modules.log import Log
+from modules.command import ProgramCommand
 
 log_firstrun = [
     Section('setup_logging', children=[
@@ -19,11 +21,33 @@ class LogModule(ModuleBase):
     """
     name = "log"
     short = "l"
+
+    commands = [
+    ProgramCommand('write', help="Write a warning to the log.",
+                        required_kwargs = {'text':"Text to log"},
+                        optional_kwargs= {'level':"Logging level: INFO / WARN / ERROR"}
+    )]
+
     def load(self):
+        self.log = Log('log_modl')
         first_run = state.root['first_run']
         first_run += log_firstrun
 
     def unload(self):
+        del self.log
+
+    def write(self, **kwargs):
+        text = kwargs.get('text')
+        level = kwargs.get('level')
+        if not level:
+            level ='info'
+        match level.lower():
+            case 'i'|'info':
+                self.log.info(text)
+            case 'w'|'warn'|'warning':
+                self.log.warning(text)
+            case 'e'|'err'|'error':
+                self.log.error(text)
         pass
 
 
