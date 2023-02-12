2fd2a3bdc2af91112ebeef87dd8abe8a496cabf9
Daniel Herkert <daniel.herkert@gmail.com>
2023-02-12 15:30:39 +0200

    exposed more

--- shadowui/__init__.py
+++ shadowui/__init__.py
@@ -1,6 +1,7 @@
 
-from .section import Section
+from .section import Section, Signal
 from .windowbase import WindowBase
 from .input import Input
 from .label import Label
 from .pageview import PageView
+from .timer import Timer
